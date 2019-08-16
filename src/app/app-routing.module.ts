5e58045433a3aafd5e3e3bda61b5da154c95d7c1
Dan H <spag1024@hotmail.com>
2019-08-16 18:24:56 -0500

    added help page and set up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from 'src/app/components/todos/todos.component'
+import { HelpComponent } from 'src/app/components/pages/help/help.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'help', component: HelpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
